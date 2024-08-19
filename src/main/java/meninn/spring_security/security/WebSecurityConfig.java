3d511f5da6216b8b40b7dbca00d6449097ac22bf
meninn <gabrielm.boff@gmail.com>
2024-08-19 13:09:48 -0300

    fix: rename package to security

--- src/main/java/meninn/spring_security/security/WebSecurityConfig.java
+++ src/main/java/meninn/spring_security/security/WebSecurityConfig.java
@@ -1,27 +1,34 @@
-package meninn.spring_security.config;
+package meninn.spring_security.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final CustomBasicAuthenticationFilter customBasicAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
+        return httpSecurity
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
                         request -> request
                                 .requestMatchers(HttpMethod.POST, "/user/**").permitAll()
                                 .anyRequest().authenticated()
-                );
-
-        return httpSecurity.build();
-
+                )
+                .addFilterBefore(customBasicAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
